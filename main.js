ae6b5ddc891a8cd381156019940aaa62d59e97f0
rutujakesare <rutujakesare38@gmail.com>
2024-03-21 00:06:05 +0530

    Request/Response

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 const http = require('http');
+const url = require('url');
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type' : 'text/plain'
-    });
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
 
-    res.write('Hello World');
-    res.end();
-}).listen(2000);
+    if (path === '/home') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome home');
+    } else if (path === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to About Us page');
+    } else if (path === '/node') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to my Node Js project');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 - Not Found');
+    }
+});
+
+const port = 9000;
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+
