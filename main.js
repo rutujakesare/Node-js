4e34fa02a9cc24a2174019445ede192a590d1b85
rutujakesare <rutujakesare38@gmail.com>
2024-03-20 19:25:30 +0530

    2 Revise js concepts

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const multiply = (a,b) => a*b;
+console.log(multiply(4,2));
+
+
+const student = {
+    name : 'Shravani',
+    age : 25,
+    greet () {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+person.greet();
