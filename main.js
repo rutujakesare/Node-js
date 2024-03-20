69311ba232594792b098be81673982ab6621cde5
rutujakesare <rutujakesare38@gmail.com>
2024-03-20 19:30:05 +0530

    3 array

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-const multiply = (a,b) => a*b;
-console.log(multiply(4,2));
-
-
-const student = {
-    name : 'Shravani',
-    age : 25,
-    greet () {
-        console.log('Hi, I am ' + this.name);
+const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+const transformedArray = array.map(item => {
+    if (item === '') {
+        return 'empty string';
+    } else {
+        return item;
     }
-};
-person.greet();
+});
+console.log(transformedArray);
