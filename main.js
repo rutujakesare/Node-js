7afdec786ff5f274922fe2ff55506a21485c815f
rutujakesare <rutujakesare38@gmail.com>
2024-03-26 01:30:28 +0530

    Writing,readingfrom file & clean the code

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
 const http = require('http');
-const url = require('url');
 
-const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
+const routes = require('./routes');
 
-    if (path === '/home') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Welcome home');
-    } else if (path === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Welcome to About Us page');
-    } else if (path === '/node') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Welcome to my Node Js project');
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 - Not Found');
-    }
-});
+console.log(routes.someText);
 
-const port = 9000;
-server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+const server = http.createServer(routes.handler);
 
-
+server.listen(8000);
